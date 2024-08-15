402859027bd3af88f023a208012dc075c95a1e2d
Lefteris Tournaris <el18845@mail.ntua.gr>
2024-08-15 17:48:42 +0300

    fix(frontend): Fix text colours

--- frontend/src/app/components/NavBar.tsx
+++ frontend/src/app/components/NavBar.tsx
@@ -1,25 +1,27 @@
 // src/app/components/NavBar.tsx
-'use client';
-import { FC } from 'react';
-import { Logo } from './Logo';
-import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
-import { Button, Chip } from '@mui/material';
-import { useRouter } from 'next/navigation';
-import { useUserContext } from '../../context/UserContext';
+'use client'
+import {FC} from 'react'
+import {Logo} from './Logo'
+import {SignInButton, SignedIn, SignedOut, UserButton} from '@clerk/nextjs'
+import {Button, Chip} from '@mui/material'
+import {useRouter} from 'next/navigation'
+import {useUserContext} from '../../context/UserContext'
 
 export const NavBar: FC = () => {
-  const router = useRouter();
-  const { credits } = useUserContext();
+  const router = useRouter()
+  const {credits} = useUserContext()
 
-  const handleClick = () => router.push('/credits');
+  const handleClick = () => router.push('/credits')
 
   return (
     <div className="h-12 w-full bg-blue-500 text-white px-4 justify-between flex items-center">
       <Logo />
       <SignedOut>
-        <Button className="text-white hover:outline hover:outline-1 hover:outline-white">
-          <SignInButton />
-        </Button>
+        <SignInButton>
+          <Button className="!text-white hover:outline hover:outline-1 hover:outline-white !capitalize">
+            Sign in
+          </Button>
+        </SignInButton>
       </SignedOut>
       <SignedIn>
         <div className="flex align-middle gap-4">
@@ -33,5 +35,5 @@ export const NavBar: FC = () => {
         </div>
       </SignedIn>
     </div>
-  );
-};
+  )
+}
