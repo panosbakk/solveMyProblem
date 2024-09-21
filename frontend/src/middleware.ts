44f6a356e9c1ff29f266eed05ed75ce4a6773b0d
Lefteris Tournaris <el18845@mail.ntua.gr>
2024-09-21 19:33:28 +0300

    fix(frontend): Fix protected routes

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,6 +1,5 @@
 import {clerkMiddleware, createRouteMatcher} from '@clerk/nextjs/server'
 
-// Should work but it doesn't
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/'])
 
 export default clerkMiddleware((auth, request) => {
@@ -10,5 +9,10 @@ export default clerkMiddleware((auth, request) => {
 })
 
 export const config = {
-  matcher: ['/((?!.*..*|_next).*)', '/', '/(api|trpc)(.*)']
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 }
